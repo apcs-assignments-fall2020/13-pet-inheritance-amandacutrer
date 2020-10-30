440d503be422037454f0327dfe1fadcdab29dd06
amandacutrer <23acutrer@athenian.org>
2020-10-30 04:36:41 +0000

    dog class

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,38 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed){
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public Dog(){
+        super("Max", 1);
+        this.breed = "Terrier";
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Bark!");
+    }
 
     // toString method
-
+    public String toString(){
+        return ("Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + breed);
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed){
+        if (breed.trim().length() != 0) {
+            this.breed = breed;
+        }
+    }
+
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,10 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog d1 = new Dog("Cooper", 6, "Miniature Pinscher");
+        System.out.println(d1.getName()); // Cooper 
+        System.out.println(d1.getAge()); // 6
+        System.out.println(d1.getBreed()); // Miniature Pinscher
+        System.out.println(d1); // Name: Cooper, Age: 6, Breed: Miniature Pinscher
+        d1.makeNoise(); // Bark!
     }
 }
